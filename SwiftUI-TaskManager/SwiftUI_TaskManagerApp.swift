33daee8239fda529962beda6248ce2a45e372e1c
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2023-01-01 18:42:04 +0300

    first commit

---
+++ SwiftUI-TaskManager/SwiftUI_TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TaskManagerApp.swift
+//  SwiftUI-TaskManager
+//
+//  Created by Mehmet Kerim ÖZEK on 1.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
