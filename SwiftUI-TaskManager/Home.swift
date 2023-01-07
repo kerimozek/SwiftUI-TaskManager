9e64d8e22038f48ffb9f4eb16bb6f9ecf83b206e
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2023-01-07 19:18:55 +0300

    refactor home screen

--- SwiftUI-TaskManager/Home.swift
+++ SwiftUI-TaskManager/Home.swift
@@ -7,37 +7,44 @@
 
 import SwiftUI
 
-
 struct Home: View {
     @StateObject var taskModel: TaskViewModel = .init()
-    // MARK: Matched Geometry NameSpace
+    // MARK: Matched Geometry Namespace
     @Namespace var animation
     
     // MARK: Fetching Task
     @FetchRequest(entity: Task.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Task.deadline, ascending: false)], predicate: nil, animation: .easeInOut) var tasks: FetchedResults<Task>
     
+    // MARK: Environment Values
+    @Environment(\.self) var env
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            VStack {
+            VStack{
                 VStack(alignment: .leading, spacing: 8) {
                     Text("Welcome Back")
                         .font(.callout)
                     Text("Here's Update Today.")
                         .font(.title2.bold())
                 }
-                .frame(maxWidth: .infinity, alignment: .leading)
+                .frame(maxWidth: .infinity,alignment: .leading)
                 .padding(.vertical)
+                
                 CustomSegmentedBar()
                     .padding(.top,5)
+                
+                // MARK: Task View
+                TaskView()
             }
             .padding()
         }
         .overlay(alignment: .bottom) {
+            // MARK: Add Button
             Button {
                 taskModel.openEditTask.toggle()
             } label: {
                 Label {
-                    Text("Add Text")
+                    Text("Add Task")
                         .font(.callout)
                         .fontWeight(.semibold)
                 } icon: {
@@ -46,8 +53,9 @@ struct Home: View {
                 .foregroundColor(.white)
                 .padding(.vertical,12)
                 .padding(.horizontal)
-                .background(.black, in: Capsule())
+                .background(.black,in: Capsule())
             }
+            // MARK: Linear Gradient BG
             .padding(.top,10)
             .frame(maxWidth: .infinity)
             .background{
@@ -71,9 +79,12 @@ struct Home: View {
     // MARK: TaskView
     @ViewBuilder
     func TaskView()->some View{
-        LazyVStack(spacing: 20) {
-            ForEach(tasks){task in
-                
+        LazyVStack(spacing: 20){
+            // MARK: Custom Filtered Request View
+            // See My Dynamic Filter Video
+            // Link in Description
+            DynamicFilteredView(currentTab: taskModel.currentTab) { (task: Task) in
+                TaskRowView(task: task)
             }
         }
         .padding(.top,20)
@@ -81,8 +92,8 @@ struct Home: View {
     
     // MARK: Task Row View
     @ViewBuilder
-    func TaskRowView(task: Task)->some View {
-        VStack(alignment: .leading, spacing: 10){
+    func TaskRowView(task: Task)->some View{
+        VStack(alignment: .leading, spacing: 10) {
             HStack{
                 Text(task.type ?? "")
                     .font(.callout)
@@ -95,10 +106,12 @@ struct Home: View {
                 
                 Spacer()
                 
-                // MARK: Edit Button Only For Non Completed Tasks
-                if !task.isCompleted{
-                    Button{
-                        
+                // MARK: Edit Button Only for Non Completed Tasks
+                if !task.isCompleted && taskModel.currentTab != "Failed"{
+                    Button {
+                        taskModel.editTask = task
+                        taskModel.openEditTask = true
+                        taskModel.setupTask()
                     } label: {
                         Image(systemName: "square.and.pencil")
                             .foregroundColor(.black)
@@ -106,34 +119,67 @@ struct Home: View {
                 }
             }
             
+            Text(task.title ?? "")
+                .font(.title2.bold())
+                .foregroundColor(.black)
+                .padding(.vertical,10)
             
+            HStack(alignment: .bottom, spacing: 0) {
+                VStack(alignment: .leading, spacing: 10) {
+                    Label {
+                        Text((task.deadline ?? Date()).formatted(date: .long, time: .omitted))
+                    } icon: {
+                        Image(systemName: "calendar")
+                    }
+                    .font(.caption)
+                    
+                    Label {
+                        Text((task.deadline ?? Date()).formatted(date: .omitted, time: .shortened))
+                    } icon: {
+                        Image(systemName: "clock")
+                    }
+                    .font(.caption)
+                }
+                .frame(maxWidth: .infinity,alignment: .leading)
+                
+                if !task.isCompleted && taskModel.currentTab != "Failed"{
+                    Button {
+                        // MARK: Updating Core Data
+                        task.isCompleted.toggle()
+                        try? env.managedObjectContext.save()
+                    } label: {
+                        Circle()
+                            .strokeBorder(.black,lineWidth: 1.5)
+                            .frame(width: 25, height: 25)
+                            .contentShape(Circle())
+                    }
+                }
+            }
         }
         .padding()
-        frame(maxWidth: .infinity)
-            .background{
-                RoundedRectangle(cornerRadius: 12, style: .continuous)
-                    .fill(Color(task.color ?? "Yellow"))
-            }
+        .frame(maxWidth: .infinity)
+        .background{
+            RoundedRectangle(cornerRadius: 12, style: .continuous)
+                .fill(Color(task.color ?? "Yellow"))
+        }
     }
-    
-    
     
     // MARK: Custom Segmented Bar
     @ViewBuilder
-    func CustomSegmentedBar()->some View {
-        let tabs = ["Today", "Upcoming", "Task Done"]
-        HStack(spacing: 10){
-            ForEach(tabs,id: \.self){ tab in
+    func CustomSegmentedBar()->some View{
+        // In Case if we Missed the Task
+        let tabs = ["Today","Upcoming","Task Done","Failed"]
+        HStack(spacing: 0){
+            ForEach(tabs,id: \.self){tab in
                 Text(tab)
                     .font(.callout)
                     .fontWeight(.semibold)
                     .scaleEffect(0.9)
-                
                     .foregroundColor(taskModel.currentTab == tab ? .white : .black)
-                    .padding(.vertical, 6)
+                    .padding(.vertical,6)
                     .frame(maxWidth: .infinity)
                     .background{
-                        if taskModel.currentTab == tab {
+                        if taskModel.currentTab == tab{
                             Capsule()
                                 .fill(.black)
                                 .matchedGeometryEffect(id: "TAB", in: animation)
