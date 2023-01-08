c08bd52bf3a0dd9dd12294aceb5d59779ae7b46a
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2023-01-08 22:10:57 +0300

    refactor taskViewModel

--- SwiftUI-TaskManager/TaskViewModel.swift
+++ SwiftUI-TaskManager/TaskViewModel.swift
@@ -9,9 +9,9 @@ import SwiftUI
 import CoreData
 
 class TaskViewModel: ObservableObject {
+    @Published var currentTab: String = "Today"
     
     // MARK: New Task Properties
-    @Published var currentTab: String = "Today"
     @Published var openEditTask: Bool = false
     @Published var taskTitle: String = ""
     @Published var taskColor: String = "Yellow"
@@ -19,29 +19,48 @@ class TaskViewModel: ObservableObject {
     @Published var taskType: String = "Basic"
     @Published var showDatePicker: Bool = false
     
+    // MARK: Editing Existing Task Data
+    @Published var editTask: Task?
+    
     // MARK: Adding Task To Core Data
-    func addTask(context: NSManagedObjectContext)-> Bool {
-        let task = Task(context: context)
+    func addTask(context: NSManagedObjectContext)->Bool{
+        // MARK: Updating Existing Data in Core Data
+        var task: Task!
+        if let editTask = editTask {
+            task = editTask
+        }else{
+            task = Task(context: context)
+        }
         task.title = taskTitle
         task.color = taskColor
         task.deadline = taskDeadline
         task.type = taskType
         task.isCompleted = false
         
-        if let _ = try? context.save() {
+        if let _ = try? context.save(){
             return true
         }
         return false
     }
     
     // MARK: Resetting Data
-    func resetTaskData() {
+    func resetTaskData(){
         taskType = "Basic"
         taskColor = "Yellow"
         taskTitle = ""
         taskDeadline = Date()
+        editTask = nil
     }
     
+    // MARK: If Edit Task Is Available then Setting Exisiting Data
+    func setupTask(){
+        if let editTask = editTask {
+            taskType = editTask.type ?? "Basic"
+            taskColor = editTask.color ?? "Yellow"
+            taskTitle = editTask.title ?? ""
+            taskDeadline = editTask.deadline ?? Date()
+        }
+    }
 }
 
 
